94506ea233a5cd15d14243cbc470cfb2b1b5161b
Vaibhav Bihani <87797047+Vaibhav525@users.noreply.github.com>
2023-08-07 13:04:52 +0530

    Create angle_dist.py

---
+++ scripts/struct_props/angle_dist.py
@@ -0,0 +1,27 @@
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from ase.io import Trajectory
+from ase.calculators.singlepoint import SinglePointCalculator
+from ase.io.trajectory import Trajectory
+import seaborn as sns, numpy as np
+
+def main(args=None):
+    Traj = ase.io.read(args.traj_path, index=':', format='extxyz')
+
+    Angle_elems=args.angle
+    A,B,C=Angle_elems.split('-')
+    analysis = Analysis(Traj)
+    Angles=analysis.get_values(analysis.get_angles(A=A,B=B,C= C, unique=True))
+    Angles_mean=np.mean(np.array(Angles),axis=0)
+    sns.displot(Angles_mean,kind="kde")
+    plt.xlabel(Angle_elems+' Angle')
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Calculate and plot RDF")
+    parser.add_argument("--traj_path", required=True, help="Path to trajectory file")
+    parser.add_argument("--angle", default='Li-P-S', help="Angle to calculate in format 'Li-P-S' ")
+    
+    
+    main(args)
