ba3d5251edcf9168aa4f7485ace61fcfce42fe97
Vaibhav Bihani <87797047+Vaibhav525@users.noreply.github.com>
2023-09-11 11:01:07 +0530

    Update dimenet_plus_plus.py

--- mdbenchgnn/models/ocp/ocpmodels/models/dimenet_plus_plus.py
+++ mdbenchgnn/models/ocp/ocpmodels/models/dimenet_plus_plus.py
@@ -36,7 +36,11 @@ import torch
 from torch import nn
 from torch_geometric.nn import radius_graph
 # from torch_geometric.nn.acts import swish
-from torch.nn.functional import relu as swish # just to bypass above errror as orch_geometric.nn.acts is depreciated
+# from torch.nn.functional import relu as swish # just to bypass above errror as orch_geometric.nn.acts is depreciated
+try:
+    from torch_geometric.nn.acts import swish
+except ImportError:
+    from torch_geometric.nn.resolver import swish
 from torch_geometric.nn.inits import glorot_orthogonal
 from torch_geometric.nn.models.dimenet import (
     BesselBasisLayer,
